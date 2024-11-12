dee6321d068473bc043fd9115ef97f531dbe26d9
ShermR6 <andrew.p.sherman21@gmail.com>
2024-11-12 16:44:37 -0600

    ArtFinish.ps1

---
+++ ArtFinish.ps1
@@ -0,0 +1,11 @@
+$magenta = "Magenta"
+$red = "Red"
+$space = " " * 12  # Increased the number of spaces for more right alignment
+
+Write-Host "`n$space╭─────────────────────────────────────╮" -ForegroundColor $red
+Write-Host "$space│            SCAN COMPLETE            │" -ForegroundColor $red
+Write-Host "$space╰─────────────────────────────────────╯" -ForegroundColor $red
+
+Write-Host "$space╭─────────────────────────────────────╮" -ForegroundColor $magenta
+Write-Host "$space│Discord @Sherm_R6 | Twitter @Sherm_R6│" -ForegroundColor $magenta
+Write-Host "$space╰─────────────────────────────────────╯" -ForegroundColor $magenta
