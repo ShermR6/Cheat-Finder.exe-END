2c2b5d8edfd3e704c4c934f8ba71e9b9ca7f549a
ShermR6 <andrew.p.sherman21@gmail.com>
2024-11-12 20:35:46 -0600

    ArtFinish.ps1

--- ArtFinish.ps1
+++ ArtFinish.ps1
@@ -6,8 +6,6 @@ Write-Host "`n$space╭───────────────────
 Write-Host "$space│            SCAN COMPLETE            │" -ForegroundColor $green
 Write-Host "$space╰─────────────────────────────────────╯" -ForegroundColor $green
 
-
-
 Write-Host "$space╭─────────────────────────────────────╮" -ForegroundColor $white
 Write-Host "$space│           Discord | @Sherm_R6       │" -ForegroundColor $white
 Write-Host "$space╰─────────────────────────────────────╯" -ForegroundColor $white
