97360c3594742c2c81715c74feb594e43ae675cb
ShermR6 <andrew.p.sherman21@gmail.com>
2024-11-12 20:31:12 -0600

    ArtFinish.ps1

--- ArtFinish.ps1
+++ ArtFinish.ps1
@@ -1,3 +1,27 @@
+function Format-Hyperlink {
+  param(
+    [Parameter(ValueFromPipeline = $true, Position = 0)]
+    [ValidateNotNullOrEmpty()]
+    [Uri] $Uri,
+
+    [Parameter(Mandatory=$false, Position = 1)]
+    [string] $Label
+  )
+
+  if (($PSVersionTable.PSVersion.Major -lt 6 -or $IsWindows) -and -not $Env:WT_SESSION) {
+    # Fallback for Windows users not inside Windows Terminal
+    if ($Label) {
+      return "$Label ($Uri)"
+    }
+    return "$Uri"
+  }
+
+  if ($Label) {
+    return "`e]8;;$Uri`e\$Label`e]8;;`e\"
+  }
+
+  return "$Uri"
+}
 Write-Output "`n`n`n $(Format-Hyperlink -Uri "https://x.com/Sherm_R6" -Label "Twitter/X | Sherm_R6")!`n`n`n"
 Write-Output "`n`n`n $(Format-Hyperlink -Uri "https://youtube.com/@Sherm_R6" -Label "YouTube | Sherm_R6")!`n`n`n"
 $green = "green"
