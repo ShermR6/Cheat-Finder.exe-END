e69588c347a7d6ee84477c6fe6b8948da78bbcad
ShermR6 <andrew.p.sherman21@gmail.com>
2024-11-12 19:04:20 -0600

    ArtFinish.ps1

--- ArtFinish.ps1
+++ ArtFinish.ps1
@@ -1,11 +1,11 @@
-$magenta = "Magenta"
-$red = "Red"
+$green = "green"
+$white = "white"
 $space = " " * 12  # Increased the number of spaces for more right alignment
 
-Write-Host "`n$space╭─────────────────────────────────────╮" -ForegroundColor $red
-Write-Host "$space│            SCAN COMPLETE            │" -ForegroundColor $red
-Write-Host "$space╰─────────────────────────────────────╯" -ForegroundColor $red
+Write-Host "`n$space╭─────────────────────────────────────╮" -ForegroundColor $green
+Write-Host "$space│            SCAN COMPLETE            │" -ForegroundColor $green
+Write-Host "$space╰─────────────────────────────────────╯" -ForegroundColor $green
 
-Write-Host "$space╭─────────────────────────────────────╮" -ForegroundColor $darkblue
-Write-Host "$space│Discord @Sherm_R6 | Twitter @Sherm_R6│" -ForegroundColor $darkblue
-Write-Host "$space╰─────────────────────────────────────╯" -ForegroundColor $darkblue
+Write-Host "$space╭─────────────────────────────────────╮" -ForegroundColor $white
+Write-Host "$space│Discord @Sherm_R6 | Twitter @Sherm_R6│" -ForegroundColor $white
+Write-Host "$space╰─────────────────────────────────────╯" -ForegroundColor $white
