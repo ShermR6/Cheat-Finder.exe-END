30b57567d1d374aaaf26d595b2c4e370ec0a7098
ShermR6 <andrew.p.sherman21@gmail.com>
2024-11-13 22:21:43 -0600

    Update ArtFinish.ps1

--- ArtFinish.ps1
+++ ArtFinish.ps1
@@ -1,15 +1,15 @@
 $green = "green"
 $white = "white"
-$magenta = "Magenta"
+$magenta = "magenta"
 $space = " " * 12  # Increased the number of spaces for more right alignment
 
 Write-Host "`n$space╭─────────────────────────────────────╮" -ForegroundColor $green
 Write-Host "$space│            SCAN COMPLETE            │" -ForegroundColor $green
 Write-Host "$space╰─────────────────────────────────────╯" -ForegroundColor $green
 
-Write-Host "$space╭─────────────────────────────────────╮" -ForegroundColor $Magenta
-Write-Host "$space│  ⌄⌄⌄ Please Consider Following ⌄⌄⌄  │" -ForegroundColor $Magenta
-Write-Host "$space╰─────────────────────────────────────╯" -ForegroundColor $Magenta
+Write-Host "$space╭─────────────────────────────────────╮" -ForegroundColor $magenta
+Write-Host "$space│  ⌄⌄⌄ Please Consider Following ⌄⌄⌄  │" -ForegroundColor $magenta
+Write-Host "$space╰─────────────────────────────────────╯" -ForegroundColor $magenta
 
 Write-Host "$space╭─────────────────────────────────────╮" -ForegroundColor $white
 Write-Host "$space│         Twitter/X | Sherm_R6        │" -ForegroundColor $white
