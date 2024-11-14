23bbd7ecd026eaabadd840c0a0888d3c174d0055
ShermR6 <andrew.p.sherman21@gmail.com>
2024-11-13 21:57:11 -0600

    ArtFinish.ps1

--- ArtFinish.ps1
+++ ArtFinish.ps1
@@ -1,14 +1,15 @@
 $green = "green"
 $white = "white"
+$darkred = "DarkRed"
 $space = " " * 12  # Increased the number of spaces for more right alignment
 
 Write-Host "`n$space╭─────────────────────────────────────╮" -ForegroundColor $green
 Write-Host "$space│            SCAN COMPLETE            │" -ForegroundColor $green
 Write-Host "$space╰─────────────────────────────────────╯" -ForegroundColor $green
 
-Write-Host "$space╭─────────────────────────────────────╮" -ForegroundColor $green
-Write-Host "$space│      Please Consider Following      │" -ForegroundColor $green
-Write-Host "$space╰─────────────────────────────────────╯" -ForegroundColor $green
+Write-Host "$space╭─────────────────────────────────────╮" -ForegroundColor $DarkRed
+Write-Host "$space│  ⌄⌄⌄ Please Consider Following ⌄⌄⌄  │" -ForegroundColor $DarkRed
+Write-Host "$space╰─────────────────────────────────────╯" -ForegroundColor $DarkRed
 
 Write-Host "$space╭─────────────────────────────────────╮" -ForegroundColor $white
 Write-Host "$space│         Twitter/X | Sherm_R6        │" -ForegroundColor $white
