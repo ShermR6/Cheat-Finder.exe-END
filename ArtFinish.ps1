ea6445be1a3dc3202147eca3fc1929a592399c7c
ShermR6 <andrew.p.sherman21@gmail.com>
2024-11-13 19:17:25 -0600

    ArtFinish.ps1

--- ArtFinish.ps1
+++ ArtFinish.ps1
@@ -6,14 +6,22 @@ Write-Host "`n$space╭───────────────────
 Write-Host "$space│            SCAN COMPLETE            │" -ForegroundColor $green
 Write-Host "$space╰─────────────────────────────────────╯" -ForegroundColor $green
 
+Write-Host "$space╭─────────────────────────────────────╮" -ForegroundColor $green
+Write-Host "$space│      Please Consider Following      │" -ForegroundColor $green
+Write-Host "$space╰─────────────────────────────────────╯" -ForegroundColor $green
+
 Write-Host "$space╭─────────────────────────────────────╮" -ForegroundColor $white
-Write-Host "$space│           Discord | @Sherm_R6       │" -ForegroundColor $white
+Write-Host "$space│         Discord | @Sherm_R6         │" -ForegroundColor $white
 Write-Host "$space╰─────────────────────────────────────╯" -ForegroundColor $white
 
 Write-Host "$space╭─────────────────────────────────────╮" -ForegroundColor $white
-Write-Host "$space│           Twitter/X | Sherm_R6      │" -ForegroundColor $white
+Write-Host "$space│         Twitter/X | Sherm_R6        │" -ForegroundColor $white
 Write-Host "$space╰─────────────────────────────────────╯" -ForegroundColor $white
 
 Write-Host "$space╭─────────────────────────────────────╮" -ForegroundColor $white
-Write-Host "$space│           YouTube | Sherm_R6        │" -ForegroundColor $white
+Write-Host "$space│         YouTube | Sherm_R6          │" -ForegroundColor $white
+Write-Host "$space╰─────────────────────────────────────╯" -ForegroundColor $white
+
+Write-Host "$space╭─────────────────────────────────────╮" -ForegroundColor $white
+Write-Host "$space│          TikTok | Sherm_R6          │" -ForegroundColor $white
 Write-Host "$space╰─────────────────────────────────────╯" -ForegroundColor $white
